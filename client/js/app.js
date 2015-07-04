d001539f552268608c8ed170fd2c3a042a0de36c
firnsy <firnsy@gmail.com>
2015-07-04 14:47:42 +1000

    added mock data for help request page.

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,39 @@
-var app = angular.module('WeCanHelpApp', ['ngMaterial']);
+var app = angular.module('WeCanHelpApp', ['ngMaterial', 'ngRoute', 'ngAnimate'])
+  .config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/register', {
+          templateUrl: 'register.html',
+          controller: 'RegisterCtrl',
+          controllerAs: 'register'
+        })
+      .when('/thanks/:reason', {
+        templateUrl: 'thanks.html',
+        controller: 'ThanksCtrl',
+        controllerAs: 'thanks'
+      });
+    }]);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.data = {
+    title: "Dereel Bushfires",
+    description: "We are looking for volunteers who can provide or assist with the following resources:",
+    resources: [
+      {
+        name: "Blankets",
+        description: "Non-flammable, non-syntetic."
+      },
+      {
+        name: "Food",
+        description: "Fresh fruit and tinned cans."
+      },
+      {
+        name: "Labour",
+        description: "Clean up of debris."
+      }
+    ]
+  };
 
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
@@ -10,5 +43,4 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) 
     { name: 'Bushfire cleanup', type: 'bushfire', where: "Dereel, Victoria, Australia", details: "Volunteers needed", active: true },
     { name: 'Earthquake relief', where: 'Wellington, New Zealand', type: 'earthquake', active: false, details: "Register for updates" }
   ];
-
 }]);
