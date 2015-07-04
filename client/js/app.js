fe0d6146b6a4c9dd9f851def59744339cbe47170
Scott Weston <scott@srw.id.au>
2015-07-04 14:46:26 +1000

    added list of current disasters mock

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,14 @@
 var app = angular.module('WeCanHelpApp', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
+
+  $scope.disasters = [
+    { name: 'Bushfire cleanup', type: 'bushfire', where: "Dereel, Victoria, Australia", details: "Volunteers needed", active: true },
+    { name: 'Earthquake relief', where: 'Wellington, New Zealand', type: 'earthquake', active: false, details: "Register for updates" }
+  ];
 
 }]);
