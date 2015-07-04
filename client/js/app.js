b12bb689829b4473c14b522133ce143a248565cb
firnsy <firnsy@gmail.com>
2015-07-04 16:46:12 +1000

    updated: move to spa.

--- client/js/app.js
+++ client/js/app.js
@@ -2,16 +2,42 @@ var app = angular.module('WeCanHelpApp', ['ngMaterial', 'ngRoute', 'ngAnimate', 
   .config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
       $routeProvider
+        .when('/', {
+          templateUrl: 'landing.html',
+          controller: 'LandingCtrl',
+          controllerAs: 'landing'
+        })
+        .when('/needed/:uuid', {
+          templateUrl: 'needed.html',
+          controller: 'NeededCtrl',
+          controllerAs: 'needed'
+        })
         .when('/register', {
           templateUrl: 'register.html',
           controller: 'RegisterCtrl',
           controllerAs: 'register'
         })
-      .when('/thanks/:reason', {
-        templateUrl: 'thanks.html',
-        controller: 'ThanksCtrl',
-        controllerAs: 'thanks'
-      });
+        .when('/request/:uuid', {
+          templateUrl: 'request.html',
+          controller: 'RequestCtrl',
+          controllerAs: 'request'
+        })
+        .when('/confirm/:uuid', {
+          templateUrl: 'confirm.html',
+          controller: 'ConfirmCtrl',
+          controllerAs: 'confirm'
+        })
+        .when('/require/:uuid', {
+          templateUrl: 'require.html',
+          controller: 'RequireCtrl',
+          controllerAs: 'require'
+        })
+        .when('/thanks/:reason', {
+          templateUrl: 'thanks.html',
+          controller: 'ThanksCtrl',
+          controllerAs: 'thanks'
+        })
+        .otherwise({redirectTo: '/'});
     }])
   .config(function(uiGmapGoogleMapApiProvider) {
     console.log("loading maps");
@@ -24,6 +50,26 @@ var app = angular.module('WeCanHelpApp', ['ngMaterial', 'ngRoute', 'ngAnimate', 
 
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
+  };
+}]);
+
+app.controller('ConfirmCtrl', ['$scope', function($scope){
+}]);
+
+app.controller('LandingCtrl', ['$scope', function($scope){
+  $scope.disasters = [
+    { name: 'Bushfire cleanup', type: 'bushfire', where: "Dereel, Victoria, Australia", details: "Volunteers needed", active: true },
+    { name: 'Earthquake relief', where: 'Wellington, New Zealand', type: 'earthquake', active: false, details: "Register for updates" }
+  ];
+}]);
+
+app.controller('NeededCtrl', ['$scope', function($scope){
+  $scope.map = { center: { latitude: -37.816215, longitude: 143.755160 }, zoom: 12 };
+}]);
+
+app.controller('RequestCtrl', ['$scope', function($scope){
   $scope.data = {
     title: "Dereel Bushfires",
     description: "We are looking for volunteers who can provide or assist with the following resources:",
@@ -42,15 +88,9 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
       }
     ]
   };
+}]);
 
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
-
-  $scope.disasters = [
-    { name: 'Bushfire cleanup', type: 'bushfire', where: "Dereel, Victoria, Australia", details: "Volunteers needed", active: true },
-    { name: 'Earthquake relief', where: 'Wellington, New Zealand', type: 'earthquake', active: false, details: "Register for updates" }
-  ];
-
+app.controller('RequireCtrl', ['$scope', function($scope){
   $scope.map = { center: { latitude: -37.816215, longitude: 143.755160 }, zoom: 12 };
 }]);
+
