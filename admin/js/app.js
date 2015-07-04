ea89884287d2b58d919923fde3ce8fbba09f20ca
Matt Weston <matt.weston@navigo.com.au>
2015-07-04 13:01:51 +1000

    simple left navigation

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('WeCanHelpApp', ['ngMaterial']);
+var app = angular.module('WeCanHelpApp', ['ngMaterial'])
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
@@ -6,3 +6,23 @@ app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   };
 
 }]);
+
+app.controller('SideNavCtrl', function($scope, $mdDialog) {
+  $scope.disasters = [
+    { name: 'Dereel', type: 'bushfire', active: true },
+    { name: 'Wellington', type: 'earthquake', active: false }
+  ];
+  $scope.actions = [
+    { name: 'Register new disaster' }
+  ];
+  $scope.itemClick = function(event) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .title('Secondary Action')
+        .content('Secondary actions can be used for one click actions')
+        .ariaLabel('Secondary click demo')
+        .ok('Neat!')
+        .targetEvent(event)
+    );
+  };
+});
